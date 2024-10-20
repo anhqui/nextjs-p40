d3171efa2c11cae407b32843e11eea01e84ed127
Qui Nguyen <qui@gmail.com>
2024-10-20 14:16:51 -0400

    first commit

--- app/(dashboard)/tickets/[id]/DeleteButton.jsx
+++ app/(dashboard)/tickets/[id]/DeleteButton.jsx
@@ -1,13 +1,29 @@
 "use client";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
+
 import { TiDelete } from "react-icons/ti";
 
 export default function DeleteButton({ id }) {
   const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
 
   const handleDelete = async () => {
     setIsLoading(true);
-    console.log("deleting id - ", id);
+    const res = await fetch(`http://localhost:3000/api/tickets/${id}`, {
+      method: "DELETE",
+    });
+
+    const result = await res.json();
+    if (result.error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+
+    if (!result.error) {
+      router.refresh();
+      router.push("/tickets");
+    }
   };
 
   return (
--- app/api/tickets/[id]/route.js
+++ app/api/tickets/[id]/route.js
@@ -1,17 +1,11 @@
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
-export async function GET(_, { params }) {
+export async function DELETE(_, { params }) {
   const id = params.id;
-  const res = await fetch(`http://localhost:4000/tickets/${id}`);
-  const ticket = await res.json();
-  if (!res.ok) {
-    return NextResponse.json(
-      { error: "Cannot find a ticket" },
-      { status: 404 }
-    );
-  }
+  const supabase = createRouteHandlerClient({ cookies });
+  const { error } = await supabase.from("Tickets").delete().eq("id", id);
 
-  return NextResponse.json(ticket, {
-    status: 200,
-  });
+  return NextResponse.json({ error });
 }
