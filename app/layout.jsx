4c29bdca46ef14beddb2dbdad9fb1f8d4faf330c
Qui Nguyen <qui@gmail.com>
2024-10-21 22:11:32 -0400

    first commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import { Rubik } from "next/font/google";
 
+export const dynamic = "force-dynamic";
+
 const rubik = Rubik({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,9 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={rubik.className}>
-        {children}
-      </body>
+      <body className={rubik.className}>{children}</body>
     </html>
   );
 }
